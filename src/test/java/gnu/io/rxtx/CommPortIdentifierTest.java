d708e5bd53ecd4e231770bec280676f20dad110c
Albrecht Schall <albrecht.schall@ise.fraunhofer.de>
2016-02-16 18:52:29 +0100

    update test suite to JUnit4

--- src/test/java/gnu/io/rxtx/CommPortIdentifierTest.java
+++ src/test/java/gnu/io/rxtx/CommPortIdentifierTest.java
@@ -1,77 +1,65 @@
-/*-------------------------------------------------------------------------
-|   RXTX License v 2.1 - LGPL v 2.1 + Linking Over Controlled Interface.
-|   RXTX is a native interface to serial ports in java.
-|   Copyright 2008 Martin Oberhuber (Wind River) and others who
-|   actually wrote it.  See individual source files for more information.
-|
-|   A copy of the LGPL v 2.1 may be found at
-|   http://www.gnu.org/licenses/lgpl.txt on March 4th 2007.  A copy is
-|   here for your convenience.
-|
-|   This library is free software; you can redistribute it and/or
-|   modify it under the terms of the GNU Lesser General Public
-|   License as published by the Free Software Foundation; either
-|   version 2.1 of the License, or (at your option) any later version.
-|
-|   This library is distributed in the hope that it will be useful,
-|   but WITHOUT ANY WARRANTY; without even the implied warranty of
-|   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-|   Lesser General Public License for more details.
-|
-|   An executable that contains no derivative of any portion of RXTX, but
-|   is designed to work with RXTX by being dynamically linked with it,
-|   is considered a "work that uses the Library" subject to the terms and
-|   conditions of the GNU Lesser General Public License.
-|
-|   The following has been added to the RXTX License to remove
-|   any confusion about linking to RXTX.   We want to allow in part what
-|   section 5, paragraph 2 of the LGPL does not permit in the special
-|   case of linking over a controlled interface.  The intent is to add a
-|   Java Specification Request or standards body defined interface in the
-|   future as another exception but one is not currently available.
-|
-|   http://www.fsf.org/licenses/gpl-faq.html#LinkingOverControlledInterface
-|
-|   As a special exception, the copyright holders of RXTX give you
-|   permission to link RXTX with independent modules that communicate with
-|   RXTX solely through the Sun Microsytems CommAPI interface version 2,
-|   regardless of the license terms of these independent modules, and to copy
-|   and distribute the resulting combined work under terms of your choice,
-|   provided that every copy of the combined work is accompanied by a complete
-|   copy of the source code of RXTX (the version of RXTX used to produce the
-|   combined work), being distributed under the terms of the GNU Lesser General
-|   Public License plus this exception.  An independent module is a
-|   module which is not derived from or based on RXTX.
-|
-|   Note that people who make modified versions of RXTX are not obligated
-|   to grant this special exception for their modified versions; it is
-|   their choice whether to do so.  The GNU Lesser General Public License
-|   gives permission to release a modified version without this exception; this
-|   exception also makes it possible to release a modified version which
-|   carries forward this exception.
-|
-|   You should have received a copy of the GNU Lesser General Public
-|   License along with this library; if not, write to the Free
-|   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-|   All trademarks belong to their respective owners.
---------------------------------------------------------------------------*/
+/*
+ * Copyright 1997-2009 by Trent Jarvi and others
+ * Copyright 1998 Kevin Hester, kevinh@acm.org
+ * Copyright 2016 Fraunhofer ISE and others
+ *
+ * This file is part of jRxTx.
+ * jRxTx is a fork of RXTX originally maintained by Trent Jarvi.
+ *
+ * jRxTx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * jRxTx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jRxTx.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package gnu.io.rxtx;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
+import org.junit.Test;
+
 import gnu.io.CommPortIdentifier;
 import gnu.io.NoSuchPortException;
-import junit.framework.TestCase;
 
-public class CommPortIdentifierTest extends TestCase {
-
-	public CommPortIdentifierTest(String testName) {
-		super(testName);
+public class CommPortIdentifierTest {
+	@Test
+	public void testGetPortIdentifiers() throws Exception {
+		List<CommPortIdentifier> l = getPortIdentifiers();
+		assertFalse("has ports", l.isEmpty());
 	}
 
-	public List<CommPortIdentifier> getPortIdentifiers() {
+	@Test(expected = NoSuchPortException.class)
+	public void testGetPortIdentifier() throws Exception {
+		List<CommPortIdentifier> l = getPortIdentifiers();
+		CommPortIdentifier first = l.get(0);
+		CommPortIdentifier last = l.get(l.size() - 1);
+		// first find by name
+		CommPortIdentifier p = CommPortIdentifier.getPortIdentifier(first.getName());
+
+		assertEquals("first found", p, first);
+
+		p = CommPortIdentifier.getPortIdentifier(last.getName());
+
+		assertEquals("last found", p, last);
+		// now the non-existent case
+
+		p = CommPortIdentifier.getPortIdentifier("wuzziwuzz");
+	}
+
+	private List<CommPortIdentifier> getPortIdentifiers() {
 		Enumeration<CommPortIdentifier> e = CommPortIdentifier.getPortIdentifiers();
 		List<CommPortIdentifier> l = new ArrayList<CommPortIdentifier>();
 
@@ -80,29 +68,4 @@ public class CommPortIdentifierTest extends TestCase {
 		}
 		return l;
 	}
-
-	public void testGetPortIdentifiers() throws Exception {
-		List<CommPortIdentifier> l = getPortIdentifiers();
-		assertFalse("has ports", l.isEmpty());
-	}
-
-	public void testGetPortIdentifier() throws Exception {
-		List<CommPortIdentifier> l = getPortIdentifiers();
-		CommPortIdentifier first = l.get(0);
-		CommPortIdentifier last = l.get(l.size() - 1);
-		// first find by name
-		CommPortIdentifier p = CommPortIdentifier.getPortIdentifier(first.getName());
-		assertEquals("first found", p, first);
-		p = CommPortIdentifier.getPortIdentifier(last.getName());
-		assertEquals("last found", p, last);
-		// now the non-existent case
-		boolean exceptionThrown = false;
-		try {
-			p = CommPortIdentifier.getPortIdentifier("wuzziwuzz");
-		} catch (NoSuchPortException e) {
-			exceptionThrown = true;
-		}
-		assertTrue("invalid port", exceptionThrown);
-	}
-
 }
