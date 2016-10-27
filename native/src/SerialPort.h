4f9102fefb4a9d33f2bbb588e11a6043e093b4c5
Albrecht Schall <albrecht.schall@ise.fraunhofer.de>
2016-10-27 16:02:29 +0200

    adding old rxtx files

---
+++ native/src/SerialPort.h
@@ -0,0 +1,1 @@
+#include "include/org_openmuc_serialio_SerialPort.h"
--- src/main/java/org/openmuc/serialio/SerialPort.java
+++ src/main/java/org/openmuc/serialio/SerialPort.java
@@ -2,4 +2,8 @@ package org.openmuc.serialio;
 
 public class SerialPort {
 
+    private native int HellWorld(String name);
+
+    private native int Bla(int foo);
+
 }
